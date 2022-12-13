48e8773ae6b312759f412241a78baa86af8ca09a
Johannes Scribante <johannes.scribs@gmail.com>
2022-12-13 14:45:56 +0200

    feat/14 - added the POST /flight-log endpoint handling to the handler

--- src/server.go
+++ src/server.go
@@ -43,7 +43,7 @@ func (s *Server) routes() {
 	//// CRUD flight log
 	s.Router.HandleFunc("/flight-log", s.prop(handler.FlightLog)).Methods("OPTIONS", "GET")
 	s.Router.HandleFunc("/flight-log/-", s.prop(handler.FlightLogs)).Methods("OPTIONS", "GET")
-	//s.Router.HandleFunc("/flight-log", s.prop(handler.CreateFlightLog)).Methods("OPTIONS", "POST")
+	s.Router.HandleFunc("/flight-log", s.prop(handler.CreateFlightLog)).Methods("OPTIONS", "POST")
 	//s.Router.HandleFunc("/flight-log/-", s.prop(handler.UpdateFlightLog)).Methods("OPTIONS", "PUT")
 	//s.Router.HandleFunc("/flight-log/-", s.prop(handler.DeleteFlightLog)).Methods("OPTIONS", "DELETE")
 }
